7bea04f8df21ef65c73f967889f5ccd2b85048c3
SalmaHassan3927 <salmahassan_3927@yahoo.com>
2017-12-23 01:23:24 +0200

    final version

--- src/Huffman/BinaryFiles.java
+++ src/Huffman/BinaryFiles.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Huffman;
 
 import java.io.File;
@@ -15,10 +11,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
-/**
- *
- * @author salma
- */
 public class BinaryFiles {
 
     public  HashMap< Byte, Integer> map = new HashMap<>();
--- src/Huffman/Node.java
+++ src/Huffman/Node.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Huffman;
 
-/**
- *
- * @author salma
- */
 public class Node {
    private int value;
    private char character;
